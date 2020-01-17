0be67451d02109927d6de00036f6ab864d664f49
Mayank Khanna <khanna98@outlook.com>
2020-01-17 12:23:56 +0530

    Removed Main Module and Adding Comments

--- 01-hello_world/hello_world.v
+++ 01-hello_world/hello_world.v
@@ -1,5 +1,10 @@
-module main
+// This is a single line comment
+// module main
+/*
+	This is a multi line comment
+	/* It can even be nested. */
+*/
 
 fn main() {
-	println('Hello World')
+	println('Hello World')	
 }
